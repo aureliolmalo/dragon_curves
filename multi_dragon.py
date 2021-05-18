c4ac2370a7e8dcafe5b4ba43c933fb41d48546d4
aureliolmalo <aurelio.lopez@walmart.com>
2021-05-18 14:29:16 -0500

    Finished first working version of multiple dragon plotter.

--- multi_dragon.py
+++ multi_dragon.py
@@ -10,9 +10,11 @@ def prep_turtle_for_next_dragon(turt:t.Turtle, new_color='red') -> t.Turtle:
     turt.penup()
     turt.setx(0)
     turt.sety(0)
+    turt.settiltangle(0)
 
-    rad_angle = math.sqrt(2)
-    deg_angle = math.degrees(rad_angle)
+    # rad_angle = math.sqrt(2)
+    # deg_angle = math.degrees(rad_angle)
+    deg_angle = 180
     turt.right(deg_angle)
     turt.color(new_color)
     turt.pendown()
@@ -21,23 +23,38 @@ def prep_turtle_for_next_dragon(turt:t.Turtle, new_color='red') -> t.Turtle:
 def build_parser() -> argparse.ArgumentParser:
     parser = dragon.build_parser()
     parser.add_argument('n_dragons', type=int, default=2)
-    return parser    
+    return parser      
 
-def get_new_color() -> tuple:
-    rgb_array = np.random.randint(0, 250, (3, ))
-    rgb_tuple = tuple(rgb_array)
-    return rgb_tuple
+def get_new_color(n_dragons:int) -> tuple:
+    # rgb_array = np.random.randint(0, 255, (3, ))
+    # rgb_tuple = tuple(rgb_array)
+    base_triple = np.random.randint(0, 256, (3, ))
+    for col in np.linspace(0.4, 1.0, num=n_dragons):
+        rgb_array = (base_triple * col).astype(np.int64)
+        rgb_tuple = tuple(rgb_array)
+        yield rgb_tuple
+
+    
 
 def main():
+    t.colormode(255)
     parser = build_parser()
     args = parser.parse_args()
-    turt = dragon.build_turtle()
-    for d in range(args.n_dragons):
-        dragon.draw_curve(turt=turt, n_steps=args.n_steps, size=args.size)
-        rgb_color = get_new_color()
+
+    colors = get_new_color(n_dragons=args.n_dragons)
+    color = next(colors)
+    
+    turt = dragon.build_turtle(color=color)
+    
+    dragon.draw_curve(turt=turt, n_steps=args.n_steps, size=args.size)
+    
+    for d in range(args.n_dragons-1):
+        rgb_color = next(colors)
         turt = prep_turtle_for_next_dragon(turt=turt, new_color=rgb_color)
-
-
+        dragon.draw_curve(turt=turt, n_steps=args.n_steps, size=args.size)
+        
+    
+    t.mainloop()
 
 if __name__ == '__main__':
     main()
